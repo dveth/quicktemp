c150c410af2306e9ee10d44d82c27d7fea4c2f65
dveth <kanosauries@gmail.com>
2023-07-19 01:09:11 -0500

    General fixes
    Error handling for inner template file retrieval
    
    Renamed variables to be more descriptive
    
    Removed unnecessary console.log statements
    
    Renamed isTemplateOccurrence to isNestedTemplate to be better descriptive

--- templater.js
+++ templater.js
@@ -1,49 +1,54 @@
 const fs = require('fs').promises
 
-function removeDuplicates(arr){
-    let uniqueArr = [...new Set(arr)];
-    return uniqueArr;
+function removeDuplicates(templateMatches){
+    let uniqueTemplateMatches = [...new Set(templateMatches)];
+    return uniqueTemplateMatches;
 }
 
 function getTemplateSubStrings(templateText){
     const regex = /{{ .* }}/g;
-    let matches = templateText.match(regex);
+    let matches = templateText.match(regex) || [];
     matches = removeDuplicates(matches);
-    console.log("Matches: " + matches);
+    //console.log("Matches: " + matches);
     return matches;
 }
 
-function isTemplateOccurrence(substring){
+function isNestedTemplate(substring){
     const regex = /% \S* %/g;
     let doesMatch = regex.test(substring);
     return doesMatch;
 }
 
-async function replaceText(templateText, substrings, arr){
+async function replaceText(templateText, substrings, templateData){
     let result = templateText;
     for(const marker of substrings){
         let inner = marker.substring(2,marker.length-2).trim();
-        if(!isTemplateOccurrence(marker)){
-            //console.log(inner + ": " + arr.hasOwnProperty(inner));
-            if(arr.hasOwnProperty(inner)){
-                result = result.replaceAll(marker, arr[inner]);
+        if(!isNestedTemplate(marker)){
+            //console.log(inner + ": " + templateData.hasOwnProperty(inner));
+            if(templateData.hasOwnProperty(inner)){
+                result = result.replaceAll(marker, templateData[inner]);
             }
         }else{
            let innerTemplate = inner.substring(2,inner.length-2).trim();
-            let templateHTML = await getTemplateHTML('templates/' + innerTemplate + '.template');
+           try{
+            const innerTemplateFilepath = 'templates/' + innerTemplate + '.template';
+            let templateHTML = await getTemplateHTML(innerTemplateFilepath);
             result = result.replaceAll(marker, templateHTML);
+           }catch (error){
+            console.error('Error while retrieving inner template ' + innerTemplateFilepath + ": " + error);
+           }
         }
     };
     return result;
 }
 
-async function getTemplateHTML (filepath, arr = {}){
-    console.log(filepath);
+async function getTemplateHTML (filepath, templateData = {}){
+    //console.log(filepath);
 
     let templateText = await fs.readFile(filepath);
     templateText = templateText.toString();
-    let substrings = getTemplateSubStrings(templateText);
-    templateText = await replaceText(templateText, substrings, arr);
+    const substrings = getTemplateSubStrings(templateText);
+    templateText = await replaceText(templateText, substrings, templateData);
     return (templateText);
 }
 
